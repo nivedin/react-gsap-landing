0d9d0e063b3fc4bac1bf425b7853d2fd34ce5a51
Nivedin <nivedinp@gmail.com>
2021-03-29 00:42:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.scss";
+import Banner from "./components/Banner";
+import Cases from "./components/Cases";
+import Header from "./components/Header";
+import IntroOverlay from "./components/IntroOverlay";
+import gsap from "gsap";
 
 function App() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    gsap.to("body", 0, { css: { visibility: "visible" } });
+
+    const tl = gsap.timeline();
+
+    tl.from(".line span", 1.8, {
+      y: 100,
+      ease: "power4.out",
+      delay: 1,
+      skeyY: 7,
+      stagger: {
+        amount: 0.3,
+      },
+    })
+      .to(".overlay-top", 1.6, {
+        height: 0,
+        ease: "expo.inOut",
+        stagger: 0.4,
+      })
+      .to(".overlay-bottom", 1.6, {
+        width: 0,
+        ease: "expo.inOut",
+        stagger: {
+          amount: 0.4,
+        },
+        delay: -0.8,
+      })
+      .to(".intro-overlay", 0, { css: { display: "none" } })
+      .from(".case-image img", 1.6, {
+        scale: 1.4,
+        ease: "expo.inOut",
+        delay: -2,
+        stagger: {
+          amount: 0.4,
+        },
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IntroOverlay />
+      <Header />
+      <Banner />
+      <Cases />
     </div>
   );
 }
